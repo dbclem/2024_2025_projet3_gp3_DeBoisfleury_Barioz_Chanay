dd1a067fa886d32733a340806d91ec9f0a6f4cc4
clemdeboisfleury <deboisfleuryclement@gmail.com>
2025-04-11 11:58:44 +0200

    ajout des assets dans tiled

---
+++ map/farm bridges free.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="farm bridges free" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="Tiny Wonder Farm Free/tilemaps/farm bridges free.png" width="192" height="176"/>
+</tileset>
