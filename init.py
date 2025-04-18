7748467de4c5614b08951f6c39039c92c3287cc5
clemdeboisfleury <168632163+dbclem@users.noreply.github.com>
2025-04-18 22:10:18 +0200

    ajout de tmx pour charger la map

--- init.py
+++ init.py
@@ -1,11 +1,13 @@
 import pygame
 from config import screen_width, screen_height
 from foods import carrot, taureau
-
+import tmx
+from pytmx import TiledMap
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 game = True
 
+tmx_data = TiledMap("map/1er_map.tmx")  # Charger la carte TMX
 
 foods = [carrot() for _ in range(10)]
 taureaus = [taureau() for _ in range(5)]
