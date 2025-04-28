d959f61234eb2cc08423230996ed6c9a5ee87963
CassandreChanay <cassandrechanay@gmail.com>
2025-04-28 15:16:49 +0200

    carte faite + ajout des éléments de la carte+ ajout de la carte au programme wip avec quelques modifs à faire

--- map/bushes.tsx
+++ map/bushes.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="bushes" tilewidth="16" tileheight="16" tilecount="6" columns="2">
- <image source="cozy-island-basic/cozy-island-basic/bushes.png" width="32" height="48"/>
+ <image source="cozy-island-basic/cozy-island-basic/bushes.aseprite" width="32" height="48"/>
 </tileset>
---
+++ map/tilseset outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilseset outside" tilewidth="16" tileheight="16" tilecount="180" columns="12">
+ <image source="../../../projet_2/2024_2025_lndb_bernard-co_gp3/Tileset 1.02/tilseset outside.png" width="192" height="240"/>
+</tileset>
