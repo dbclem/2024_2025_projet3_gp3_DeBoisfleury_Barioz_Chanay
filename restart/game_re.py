825deb3af1e6c00a54657152df22ec912aa7cc8b
clemdeboisfleury <168632163+dbclem@users.noreply.github.com>
2025-05-07 23:16:13 +0200

    restart du projet

---
+++ restart/game_re.py
@@ -0,0 +1,34 @@
+import pygame
+import pytmx  
+import pyscroll
+import pytmx.util_pygame
+
+class Game : 
+    def __init__(self):
+        
+
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Game")
+
+        tmx_data = pytmx.util_pygame.load_pygame("map\map_1er_jet.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+
+
+    def run(self):
+
+        running = True
+        
+        while running:
+        
+            self.groupe.draw(self.screen)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+        pygame.quit()
---
+++ restart/main_re.py
@@ -0,0 +1,7 @@
+import pygame 
+from game_re import Game
+
+if __name__ == "__main__":
+    pygame.init()
+    game = Game()
+    game.run() 
